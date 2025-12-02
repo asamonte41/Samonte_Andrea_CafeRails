ccc65cdee74700e5dc2e25bb52aa3b3d0dca8038
asamonte41 <asamonte41@academic.rrc.ca>
2025-12-02 15:00:11 -0600

    Edited errors with merging

--- db/schema.rb
+++ db/schema.rb
@@ -23,9 +23,9 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_30_231707) do
     t.bigint "resource_id"
     t.string "resource_type"
     t.datetime "updated_at", null: false
-    t.index [ "author_type", "author_id" ], name: "index_active_admin_comments_on_author"
-    t.index [ "namespace" ], name: "index_active_admin_comments_on_namespace"
-    t.index [ "resource_type", "resource_id" ], name: "index_active_admin_comments_on_resource"
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -34,8 +34,8 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_30_231707) do
     t.string "name", null: false
     t.bigint "record_id", null: false
     t.string "record_type", null: false
-    t.index [ "blob_id" ], name: "index_active_storage_attachments_on_blob_id"
-    t.index [ "record_type", "record_id", "name", "blob_id" ], name: "index_active_storage_attachments_uniqueness", unique: true
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
@@ -47,13 +47,13 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_30_231707) do
     t.string "key", null: false
     t.text "metadata"
     t.string "service_name", null: false
-    t.index [ "key" ], name: "index_active_storage_blobs_on_key", unique: true
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
-    t.index [ "blob_id", "variation_digest" ], name: "index_active_storage_variant_records_uniqueness", unique: true
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "admin_users", force: :cascade do |t|
@@ -64,8 +64,8 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_30_231707) do
     t.datetime "reset_password_sent_at"
     t.string "reset_password_token"
     t.datetime "updated_at", null: false
-    t.index [ "email" ], name: "index_admin_users_on_email", unique: true
-    t.index [ "reset_password_token" ], name: "index_admin_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
   create_table "categories", force: :cascade do |t|
@@ -91,13 +91,8 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_30_231707) do
     t.bigint "product_id", null: false
     t.integer "quantity", default: 1, null: false
     t.datetime "updated_at", null: false
-<<<<<<< HEAD
     t.index ["order_id"], name: "index_order_items_on_order_id"
     t.index ["product_id"], name: "index_order_items_on_product_id"
-=======
-    t.index [ "order_id" ], name: "index_order_items_on_order_id"
-    t.index [ "product_id" ], name: "index_order_items_on_product_id"
->>>>>>> master
   end
 
   create_table "orders", force: :cascade do |t|
@@ -118,15 +113,9 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_30_231707) do
     t.integer "total_cents", default: 0, null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-<<<<<<< HEAD
     t.index ["customer_id"], name: "index_orders_on_customer_id"
     t.index ["province_id"], name: "index_orders_on_province_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
-=======
-    t.index [ "customer_id" ], name: "index_orders_on_customer_id"
-    t.index [ "province_id" ], name: "index_orders_on_province_id"
-    t.index [ "user_id" ], name: "index_orders_on_user_id"
->>>>>>> master
   end
 
   create_table "pages", force: :cascade do |t|
@@ -135,11 +124,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_30_231707) do
     t.string "slug"
     t.string "title"
     t.datetime "updated_at", null: false
-<<<<<<< HEAD
     t.index ["slug"], name: "index_pages_on_slug", unique: true
-=======
-    t.index [ "slug" ], name: "index_pages_on_slug", unique: true
->>>>>>> master
   end
 
   create_table "products", force: :cascade do |t|
@@ -153,38 +138,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_30_231707) do
     t.boolean "recently_updated"
     t.integer "stock"
     t.datetime "updated_at", null: false
-    t.index [ "category_id" ], name: "index_products_on_category_id"
-  end
-
-  create_table "provinces", force: :cascade do |t|
-    t.string "abbreviation", null: false
-    t.datetime "created_at", null: false
-    t.integer "gst_cents", default: 0, null: false
-    t.integer "hst_cents", default: 0, null: false
-    t.string "name", null: false
-    t.integer "pst_cents", default: 0, null: false
-    t.datetime "updated_at", null: false
-    t.index [ "abbreviation" ], name: "index_provinces_on_abbreviation", unique: true
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "address"
-    t.string "city"
-    t.datetime "created_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "first_name"
-    t.string "last_name"
-    t.string "postal"
-    t.bigint "province_id"
-    t.datetime "remember_created_at"
-    t.datetime "reset_password_sent_at"
-    t.string "reset_password_token"
-    t.integer "role", default: 0, null: false
-    t.datetime "updated_at", null: false
-    t.index [ "email" ], name: "index_users_on_email", unique: true
-    t.index [ "province_id" ], name: "index_users_on_province_id"
-    t.index [ "reset_password_token" ], name: "index_users_on_reset_password_token", unique: true
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "provinces", force: :cascade do |t|
